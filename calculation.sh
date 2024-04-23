3fec5d8f3ae9136b4b4b82ef3b57614efa24c994
sandeepveggalam124 <sandeepveggalam124@gmail.com>
2024-04-23 17:12:28 +0530

    first1

--- calculation.sh
+++ calculation.sh
@@ -2,5 +2,16 @@
 
 # Function for addition
 addition() {
-    echo "$1 + $2" | bc
+    sum=$(($1 + $2))
+    echo "The sum of $1 and $2 is: $sum"
 }
+
+# Main script
+echo "Welcome to the addition calculator!"
+
+# Input the numbers
+read -p "Enter the first number: " num1
+read -p "Enter the second number: " num2
+
+# Call the addition function
+addition $num1 $num2
